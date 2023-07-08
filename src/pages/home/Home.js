28f9cc0f7c34b1076b97f4722ff1b6faf7df6d39
purushottam bondre <purushottam.bondre@sumasoft.net>
2023-07-08 08:45:02 +0530

    added styles

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import useStyles from "./homeStyles";
 
 export const Home = () => {
-  return <>Welcome to elRed</>;
+  const { classes } = useStyles();
+  return <div className={classes.pageLabel}>Welcome to elRed</div>;
 };
---
+++ src/pages/home/homeStyles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from "tss-react/mui";
+
+export default makeStyles()((theme) => ({
+  pageLabel: {
+    fontWeight: "bold",
+  },
+}));
